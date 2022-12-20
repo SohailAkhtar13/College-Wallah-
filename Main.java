5d6c48dcd94064cb7e209302da14db2f33b65a49
Casper <45950023+SohailAkhtar13@users.noreply.github.com>
2022-12-21 05:17:22 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    static int factorial(int n)
+    {
+        if (n==0) return 1;
+
+       return  n* factorial(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(factorial(n));
+
+
+    }
+}
