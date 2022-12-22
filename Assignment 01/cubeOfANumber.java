41358e9f4bc0f0f92659e374d6bec5998eb4d439
Casper <45950023+SohailAkhtar13@users.noreply.github.com>
2022-12-22 09:26:35 +0530

    Create cubeOfANumber.java

---
+++ Assignment 01/cubeOfANumber.java
@@ -0,0 +1,11 @@
+//Q4 - Write a Java program to calculate the cube of a number. (Easy)
+//Sample Input : 4
+//Sample Output : 64
+public class Main {
+    public static void main(String[] args) {
+        int num=4;
+        int cube=num*num*num;
+        System.out.println("The cube is "+cube);
+
+    }
+}
