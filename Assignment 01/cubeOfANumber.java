21d5ae53705502362a17655d223632f934d461e9
Casper <45950023+SohailAkhtar13@users.noreply.github.com>
2022-12-22 09:26:58 +0530

    Update cubeOfANumber.java

--- Assignment 01/cubeOfANumber.java
+++ Assignment 01/cubeOfANumber.java
@@ -1,6 +1,7 @@
 //Q4 - Write a Java program to calculate the cube of a number. (Easy)
 //Sample Input : 4
 //Sample Output : 64
+
 public class Main {
     public static void main(String[] args) {
         int num=4;
