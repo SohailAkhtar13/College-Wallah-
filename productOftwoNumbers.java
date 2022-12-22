6ec9048385ed2f7513422e11679730fdc4a874f4
Casper <45950023+SohailAkhtar13@users.noreply.github.com>
2022-12-22 09:02:39 +0530

    Update productOftwoNumbers.java

--- productOftwoNumbers.java
+++ productOftwoNumbers.java
@@ -1,15 +1,1 @@
-//Q1 - Take 2 integer values in two variables x and y and print their product. (Easy)
-//Sample Input : x=2, y=4 (Both integers)
-//Sample Output : 8
 
-public class Main {
-
-    public static void main(String[] args) {
-        int x=2,y=4;
-        int product=x*y;
-        
-        System.out.println("The product is "+product);
-
-
-    }
-}
