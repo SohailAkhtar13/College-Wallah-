6c2cd3d38a09684315db0aafeecdb3371d82418e
Casper <45950023+SohailAkhtar13@users.noreply.github.com>
2022-12-22 09:16:14 +0530

    Create printASCIIValue.java

---
+++ Assignment 01/printASCIIValue.java
@@ -0,0 +1,11 @@
+//Q2 - Print the ASCII value of character ‘U’. (Easy)
+//Sample Input : already mentioned as ‘U’
+//Sample Output : 85
+
+public class Main {
+    public static void main(String[] args) {
+        int u='U';
+
+        System.out.println("The ASCII value is "+u);
+    }
+}
